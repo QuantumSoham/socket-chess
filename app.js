e18387e4f898e40d17e6a7538f738230ca6021d7
QuantumSoham <quantumsc03@gmail.com>
2026-03-15 22:06:08 +0530

    initialized project structure

---
+++ app.js
@@ -0,0 +1,31 @@
+const express= require("express");
+const socket =require("socket.io");
+const http=require("http");
+const {Chess} =require("chess.js"); //Chess class in js
+const path=require("path");
+
+const app=express();
+const server=http.createServer(app);
+//instantiate socket io in http server
+const io=socket(server);//link http server with socket 
+
+//initialize chess
+const chess=new Chess(); //from chess js
+let players={};
+let currentPlayer="white";
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+io.on("connection", function(uniquesocket)
+{
+    console.log("connected");
+});
+ 
+server.listen(3000,function(){
+    console.log("listening on port 3000");
+});
